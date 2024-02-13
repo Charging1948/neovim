36db4de83049e72c3a897327265024f80425f868
jk <joachim.dev@pm.me>
2024-02-13 14:13:29 +0100

    add luasnippets and enable friendly-snippets

--- modules/nixvim/luasnip/default.nix
+++ modules/nixvim/luasnip/default.nix
@@ -1,3 +1,23 @@
-{...}: {
-  plugins.luasnip.enable = true;
+_: {
+  plugins = {
+    friendly-snippets = {enable = true;};
+    luasnip = {
+      enable = true;
+      fromLua = [{paths = "./snippets";}];
+      fromVscode = [{}];
+      extraConfig = {
+        history = true;
+
+        updateevents = "TextChanged,TextChangedI";
+
+        enable_autosnippets = true;
+
+        ext_opts = {
+          "types.choiceNode" = {
+            active = {virt_text = [["<-" "Error"]];};
+          };
+        };
+      };
+    };
+  };
 }
