b134abdfbcc5346f2f3af5834e8e9d31ad7c9b2a
jk <joachim.dev@pm.me>
2024-02-26 02:30:58 +0100

    enable treesitter extras

---
+++ modules/nixvim/treesitter-extras/default.nix
@@ -0,0 +1,11 @@
+{
+  plugins.treesitter-refactor = {
+    enable = true;
+    highlightCurrentScope.enable = true;
+    highlightDefinitions.enable = true;
+    navigation.enable = true;
+    smartRename.enable = true;
+  };
+  plugins.treesitter-context = { enable = true; };
+  plugins.treesitter-textobjects.lspInterop.enable = true;
+}
