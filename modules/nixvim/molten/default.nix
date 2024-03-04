8130f4c0b39dc29ab8ea31444e419830101b5784
jk <joachim.dev@pm.me>
2024-03-04 15:40:34 +0100

    fetch molten-nvim manually to provide it with necessary python dependencies

--- modules/nixvim/molten/default.nix
+++ modules/nixvim/molten/default.nix
@@ -1,6 +1,34 @@
-{ pkgs, ... }: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   plugins.molten = {
     enable = true;
-    settings = { image_provider = "image.nvim"; };
+    package = pkgs.vimUtils.buildVimPlugin {
+      pname = "molten-nvim";
+      version = "2024-02-23";
+      src = pkgs.fetchFromGitHub {
+        owner = "benlubas";
+        repo = "molten-nvim";
+        rev = "8346bba69e0de96278dad2038e9be74605908b7d";
+        # sha256 = lib.fakeSha256;
+        sha256 = "08f3zxzka43f87fks56594476h57yq01x7a1zdsn4acc278xg1nb";
+      };
+      passthru.python3Dependencies = ps:
+        with ps; [
+          pynvim
+          jupyter-client
+          cairosvg
+          ipython
+          nbformat
+        ];
+      meta.homepage = "https://github.com/benlubas/molten-nvim/";
+    };
+    settings = {image_provider = "image.nvim";};
   };
+
+  # extraConfigLuaPost = ''
+  #   require('molten')
+  # '';
 }
