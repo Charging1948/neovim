144aafb2bae570e09eaf3bbe5a62f5b45732c59a
jk <joachim.dev@pm.me>
2024-03-04 09:23:10 +0100

    enable kitty font size increase in zen mode

--- modules/nixvim/zen-mode/default.nix
+++ modules/nixvim/zen-mode/default.nix
@@ -1,3 +1,11 @@
 {pkgs, ...}: {
   extraPlugins = with pkgs.vimPlugins; [twilight-nvim zen-mode-nvim];
+
+  extraConfigLuaPost = ''
+    require('zen-mode').setup({
+      plugins = {
+        kitty = { enabled = true },
+      }
+    })
+  '';
 }
