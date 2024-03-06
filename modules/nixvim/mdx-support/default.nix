c60d576188f7960c43d1c137a17ea78548172696
jk <joachim.dev@pm.me>
2024-03-06 12:12:41 +0100

    add vim-mdx-js

---
+++ modules/nixvim/mdx-support/default.nix
@@ -0,0 +1,18 @@
+{pkgs, ...}: {
+  extraPlugins = with pkgs; [
+    (pkgs.vimUtils.buildVimPlugin {
+      pname = "vim-mdx-js";
+      version = "2024-03-06";
+      src = pkgs.fetchFromGitHub {
+        owner = "jxnblk";
+        repo = "vim-mdx-js";
+        rev = "17179d7f2a73172af5f9a8d65b01a3acf12ddd50";
+        # sha256 = lib.fakeSha256;
+        sha256 = "wfYCvw9JVGG8p8PQhRPT6CeGGf2OVz9SR2KQM0LjQhY=";
+      };
+      meta.homepage = "https://github.com/jxnblk/vim-mdx-js";
+    })
+  ];
+
+  filetype = {extension.mdx = "markdown.mdx";};
+}
