6432694f250b5aa8045a0e1c77a99f27009142ee
jk <joachim.dev@pm.me>
2024-03-16 13:19:42 +0100

    update harpoon config

--- modules/nixvim/harpoon/default.nix
+++ modules/nixvim/harpoon/default.nix
@@ -1,10 +1,29 @@
-_: {
+{pkgs, ...}: {
   plugins.harpoon = {
     enable = true;
+    # package = pkgs.vimPlugins.harpoon2;
     enableTelescope = true;
     keymaps = {
       addFile = "<leader>a";
-      cmdToggleQuickMenu = "<C-e>";
+      toggleQuickMenu = "<C-e>";
+      navNext = "<leader>hn";
+      navPrev = "<leader>hp";
+      tmuxGoToTerminal = {
+        "1" = "<C-1>";
+        "2" = "<C-2>";
+      };
     };
   };
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>fm";
+      action = "<cmd>:Telescope harpoon marks<cr>";
+      options = {
+        silent = true;
+        desc = "[F]ind Harpoon [M]arks";
+      };
+    }
+  ];
 }
