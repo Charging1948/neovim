e61eaca42a0c687d238fb58be1275f7bc57d9401
jk <joachim.dev@pm.me>
2024-03-16 13:19:34 +0100

    update quarto config

--- modules/nixvim/quarto/default.nix
+++ modules/nixvim/quarto/default.nix
@@ -11,7 +11,8 @@ with lib.plusultra; {
     extraPackages = with pkgs; [quarto typst typstfmt typst-lsp typst-live];
 
     extraConfigLuaPost = ''
-      require('otter').setup({
+      local otter = require('otter')
+      otter.setup({
         buffers = {
           -- if set to true, the filetype of the otterbuffers will be set.
           -- otherwise only the autocommand of lspconfig that attaches
@@ -19,6 +20,19 @@ with lib.plusultra; {
           set_filetype = true,
         },
       })
+      -- table of embedded languages to look for.
+      -- required (no default)
+      local languages = {'python', 'lua', 'r' }
+
+      -- enable completion/diagnostics
+      -- defaults are true
+      local completion = true
+      local diagnostics = true
+      -- treesitter query to look for embedded languages
+      -- uses injections if nil or not set
+      local tsquery = nil
+
+      otter.activate(languages, completion, diagnostics, tsquery)
 
       require('quarto').setup({
         lspFeatures = {
