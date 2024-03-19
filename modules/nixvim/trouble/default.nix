6a688503619e1416fe580326178de79ef928ab36
jk <joachim.dev@pm.me>
2024-03-19 21:01:51 +0100

    fix trouble module settings

--- modules/nixvim/trouble/default.nix
+++ modules/nixvim/trouble/default.nix
@@ -1,42 +1,38 @@
-{lib, ...}:
+{ lib, ... }:
 with lib.plusultra; {
-  keymaps = [
-    {
-      mode = "n";
-      key = "<leader>tt";
-      action = "<cmd>:TroubleToggle<cr>";
-      options = {
-        silent = true;
-        desc = "Toggle trouble";
-      };
-    }
-  ];
+  keymaps = [{
+    mode = "n";
+    key = "<leader>tt";
+    action = "<cmd>:TroubleToggle<cr>";
+    options = {
+      silent = true;
+      desc = "Toggle trouble";
+    };
+  }];
 
   plugins = {
     trouble = {
       enable = true;
-
-      autoClose = true;
-      autoPreview = true;
-      useDiagnosticSigns = true;
-
-      extraOptions = {
+      settings = {
+        auto_close = true;
+        auto_preview = true;
+        use_diagnostic_signs = true;
         action_keys = {
           close = "q";
           cancel = "<esc>";
           refresh = "r";
-          jump = ["<cr>" "<tab>"];
-          open_split = ["S"];
-          open_vsplit = ["s"];
-          open_tab = ["<c-t>"];
-          jump_close = ["o"];
-          toggle_mode = ["m"];
-          toggle_preview = ["P"];
+          jump = [ "<cr>" "<tab>" ];
+          open_split = [ "S" ];
+          open_vsplit = [ "s" ];
+          open_tab = [ "<c-t>" ];
+          jump_close = [ "o" ];
+          toggle_mode = [ "m" ];
+          toggle_preview = [ "P" ];
           hover = "K";
           preview = "p";
-          close_folds = ["zC" "zc"];
-          open_folds = ["zO" "zo"];
-          toggle_fold = ["zA" "za"];
+          close_folds = [ "zC" "zc" ];
+          open_folds = [ "zO" "zo" ];
+          toggle_fold = [ "zA" "za" ];
           previous = "k";
           next = "j";
         };
