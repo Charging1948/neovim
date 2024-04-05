1510c101759240e36f90dd97b87b75572293c6df
jk <joachim.dev@pm.me>
2024-04-05 12:39:58 +0200

    add dap extensions and keymaps

--- modules/nixvim/dap/default.nix
+++ modules/nixvim/dap/default.nix
@@ -1,12 +1,27 @@
-{...}: {
+{ ... }: {
   plugins = {
     dap = {
       enable = true;
 
       extensions = {
         dap-ui.enable = true;
+        dap-go.enable = true;
+        dap-python.enable = true;
         dap-virtual-text.enable = true;
       };
     };
   };
+
+  keymaps = [
+    {
+      key = "<leader>dt";
+      action = "<cmd>DapToggleBreakpoint<CR>";
+      options.desc = "Toggle Breakpoint";
+    }
+    {
+      key = "<leader>dc";
+      action = "<cmd>DapContinue<CR>";
+      options.desc = "Dap Continue";
+    }
+  ];
 }
