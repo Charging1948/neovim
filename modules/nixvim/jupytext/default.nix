7bdc9670939090ca86b6a8f2f2fb323ea1bd8dfa
jk <joachim.dev@pm.me>
2024-04-05 11:49:52 +0200

    update jupytext module declaration to rather use builtin module, now that it is available

--- modules/nixvim/jupytext/default.nix
+++ modules/nixvim/jupytext/default.nix
@@ -1,34 +1,34 @@
 { pkgs, ... }:
-let pyPacks = p: with p; [ jupytext ];
-in {
-  extraPlugins = with pkgs;
-    [
-      (pkgs.vimUtils.buildVimPlugin {
-        pname = "jupytext-nvim";
-        version = "2024-03-04";
-        src = pkgs.fetchFromGitHub {
-          owner = "GCBallesteros";
-          repo = "jupytext.nvim";
-          rev = "68fddf28119dbaddfaea6b71f3d6aa1e081afb93";
-          # sha256 = lib.fakeSha256;
-          sha256 = "x5emW+qfUTUDR72B9QdDgVdrb8wGH9D7AdtRrQm80sI=";
-        };
-        passthru.python3Dependencies = pyPacks;
-        meta.homepage = "https://github.com/GCBallesteros/jupytext.nvim";
-      })
-    ];
-
+let
+  pyPacks = p: with p; [ jupytext ];
+  jupytext-package = pkgs.vimUtils.buildVimPlugin {
+    pname = "jupytext-nvim";
+    version = "2024-03-04";
+    src = pkgs.fetchFromGitHub {
+      owner = "GCBallesteros";
+      repo = "jupytext.nvim";
+      rev = "68fddf28119dbaddfaea6b71f3d6aa1e081afb93";
+      # sha256 = lib.fakeSha256;
+      sha256 = "x5emW+qfUTUDR72B9QdDgVdrb8wGH9D7AdtRrQm80sI=";
+    };
+    passthru.python3Dependencies = pyPacks;
+    meta.homepage = "https://github.com/GCBallesteros/jupytext.nvim";
+  };
+in
+{
   extraPython3Packages = pyPacks;
 
-  extraConfigLuaPost = ''
-    require('jupytext').setup({
+  plugins.jupytext = {
+    enable = true;
+    package = jupytext-package;
+    settings = {
       custom_language_formatting = {
         python = {
-            extension = "qmd",
-            style = "quarto",
-            force_ft = true, -- you can set whatever filetype you want here
-        },
-      }
-    })
-  '';
+          extension = "qmd";
+          style = "quarto";
+          force_ft = true;
+        };
+      };
+    };
+  };
 }
